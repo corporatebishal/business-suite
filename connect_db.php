e6e96a5a3d3f796e8ae4e4103c44ac65da4b4856
Bishal Shrestha <bishal.revenger@gmail.com>
2024-06-08 13:36:49 +1000

    Delete connect_db.php

--- connect_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Connect to the MySQL server
-$conn = mysqli_connect("mysql.bishal.com.au", "bishaldb", "Supple@135#");
-
-// Select the database
-mysqli_select_db($conn, "bishaldb");
-
-?>
